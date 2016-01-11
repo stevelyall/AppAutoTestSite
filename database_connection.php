7758628489ceda9a6fd8cd7e6e47766106a3de9a
Steve <stevenlyall@gmail.com>
2016-01-11 10:37:59 -0800

    remove condition for db credentials

--- database_connection.php
+++ database_connection.php
@@ -3,22 +3,20 @@
 //returns a database connection
 function connectToDb()
 {
+
 	// db for production
-	if ($_SERVER['SERVER_NAME'] == '198.162.21.11') {
 		$host = "127.0.0.1";
 		$user = "appautotest";
 		$pass = "mobileappstru";
 		$dbname = "appautotest";
 		$port = "3306";
-	}
+
 	// development
-	else {
-		$host = "127.0.0.1";
-		$user = "root";
-		$pass = "";
-		$dbname = "appautotest";
-		$port = "3306";
-	}
+//		$host = "127.0.0.1";
+//		$user = "root";
+//		$pass = "";
+//		$dbname = "appautotest";
+//		$port = "3306";
 
 	// connect to the database
 	$connection = mysqli_connect($host, $user, $pass, $dbname, $port);
