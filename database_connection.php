a1c268bd218ab82f547aa8a48651ba57145d9f59
Steve <stevenlyall@gmail.com>
2016-01-11 16:45:54 -0800

    results of script shown

--- database_connection.php
+++ database_connection.php
@@ -5,18 +5,18 @@ function connectToDb()
 {
 
 	// db for production
-		$host = "127.0.0.1";
-		$user = "appautotest";
-		$pass = "mobileappstru";
-		$dbname = "appautotest";
-		$port = "3306";
-
-	// development
 //		$host = "127.0.0.1";
-//		$user = "root";
-//		$pass = "";
+//		$user = "appautotest";
+//		$pass = "mobileappstru";
 //		$dbname = "appautotest";
 //		$port = "3306";
+
+	// development
+	$host = "127.0.0.1";
+	$user = "root";
+	$pass = "";
+	$dbname = "appautotest";
+	$port = "3306";
 
 	// connect to the database
 	$connection = mysqli_connect($host, $user, $pass, $dbname, $port);
